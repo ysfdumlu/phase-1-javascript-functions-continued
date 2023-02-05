6647a58873850d2c36398dcdd0b56ea2292f65c0
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-05 06:00:34 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun());
+  
+  function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`;
+  }
+  console.log(mondayWork());
+  
+  function wrapAdjective(symbol="*") {
+    return ((adjective="special") => `You are ${symbol}${adjective}${symbol}!`);
+  }
+  console.log(wrapAdjective("99")("superduper"));
+  
